6bd27453979f4b350c4c0a09d1cb57a18537d25e
brickhu <brickhu@126.com>
2022-03-10 20:25:25 +0800

    导航

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.less';
+import './App.scss';
 // import { Button } from 'antd';
 import {BrowserRouter, Route, Routes, useLocation, useNavigate} from 'react-router-dom';
 import {AppHeader,Sidebar} from './components/app-header';
 import Home from './pages/home';
-import { Linkitem,LinkGroup } from './components/linkitems';
 import { useMediaQuery } from 'react-responsive'
 
 
 
 function App() {
-  const isBigScreen = useMediaQuery({ query: '(min-width: 1024px)' })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 960px)' })
   return (
-    <div className={isBigScreen ? "desktop-layout App" : "mobile-layout App"}>
+    <div className={isBigScreen?"App desktop_layout":"App mobile_layout"}>
       {isBigScreen ? <Sidebar /> : <AppHeader />}
-      <div className="app-body">
+      <div className="app_body">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
